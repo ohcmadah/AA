217b636361d72c77d512f55d742eebc5be102f7e
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-08-22 00:14:45 +0900

    pager adapter 생성

--- app/src/main/java/com/ninecm/aa/adapter/MainAdapterPager.java
+++ app/src/main/java/com/ninecm/aa/adapter/MainAdapterPager.java
@@ -1,4 +1,36 @@
 package com.ninecm.aa.adapter;
 
-public class MainAdapterPager {
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import com.ninecm.aa.fragment.RankingFragment;
+import com.ninecm.aa.fragment.TimeFragment;
+
+public class MainAdapterPager extends FragmentStatePagerAdapter {
+    public static final int COUNT = 2;
+
+    public MainAdapterPager(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                TimeFragment timeFragment = new TimeFragment();
+                return timeFragment;
+            case 1:
+                RankingFragment rankingFragment = new RankingFragment();
+                return rankingFragment;
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return COUNT;
+    }
 }
