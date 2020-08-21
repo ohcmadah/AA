b861a1029733bfe67bf1ed810242a1b7231c61f6
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-08-22 00:16:10 +0900

    Navigation 구현

--- app/src/main/java/com/ninecm/aa/MainActivity.java
+++ app/src/main/java/com/ninecm/aa/MainActivity.java
@@ -1,14 +1,37 @@
 package com.ninecm.aa;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+import com.ninecm.aa.adapter.MainAdapterPager;
+
 public class MainActivity extends AppCompatActivity {
+
+    private MainAdapterPager adapterPager;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //init();
+        //setUp();
+    }
+
+    private void setUp() {
+        tabLayout.setupWithViewPager(viewPager);
+
+        adapterPager = new MainAdapterPager(getSupportFragmentManager());
+        viewPager.setAdapter(adapterPager);
+    }
+
+    private  void init() {
+        viewPager = findViewById(R.id.viewpager);
+        tabLayout = findViewById(R.id.tab_layout);
     }
 }
