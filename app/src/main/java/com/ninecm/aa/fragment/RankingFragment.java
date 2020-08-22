69785649918e61a9886d2d7b2589ecdd30a24e72
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-08-22 19:52:56 +0900

    onCreate 삭제

--- app/src/main/java/com/ninecm/aa/fragment/RankingFragment.java
+++ app/src/main/java/com/ninecm/aa/fragment/RankingFragment.java
@@ -12,11 +12,6 @@ import androidx.fragment.app.Fragment;
 import com.ninecm.aa.R;
 
 public class RankingFragment extends Fragment {
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
