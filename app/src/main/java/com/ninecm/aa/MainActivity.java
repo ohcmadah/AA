7ffa318fe7c4666500341bd58aa83154a588ea9e
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-08-22 21:51:52 +0900

    RecyclerView 설정 삭제

--- app/src/main/java/com/ninecm/aa/MainActivity.java
+++ app/src/main/java/com/ninecm/aa/MainActivity.java
@@ -1,27 +1,18 @@
 package com.ninecm.aa;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
 import com.google.android.material.tabs.TabLayout;
 import com.ninecm.aa.adapter.MainAdapterPager;
-import com.ninecm.aa.adapter.TimeItemAdapter;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private MainAdapterPager adapterPager;
-    private RecyclerView.Adapter timeItemAdapter;
     private ViewPager viewPager;
     private TabLayout tabLayout;
-    private RecyclerView timeRecyclerView;
-    List<String> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +26,6 @@ public class MainActivity extends AppCompatActivity {
     private  void init() {
         viewPager = (ViewPager) findViewById(R.id.viewpager);
         tabLayout = (TabLayout) findViewById(R.id.tab_layout);
-        timeRecyclerView = (RecyclerView) findViewById(R.id.time_recyclerview);
-        list = new ArrayList<>();
     }
 
     private void setUp() {
@@ -47,20 +36,6 @@ public class MainActivity extends AppCompatActivity {
 
         viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.addOnTabSelectedListener(changePages);
-
-        list.add("1");
-        list.add("2");
-        list.add("3");
-        list.add("4");
-        list.add("5");
-        list.add("6");
-        list.add("7");
-        list.add("8");
-        list.add("9");
-
-        timeItemAdapter = new TimeItemAdapter(list);
-        timeRecyclerView.setAdapter(timeItemAdapter);
-        timeRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
     TabLayout.OnTabSelectedListener changePages = new TabLayout.OnTabSelectedListener() {
