3cba8f58e53597ba01a42afda01d89e9c18e2e10
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-08-22 21:55:17 +0900

    TimeFragment에 RecyclerView 설정

--- app/src/main/java/com/ninecm/aa/fragment/TimeFragment.java
+++ app/src/main/java/com/ninecm/aa/fragment/TimeFragment.java
@@ -8,14 +8,40 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.ninecm.aa.R;
+import com.ninecm.aa.adapter.TimeItemAdapter;
+
+import java.util.List;
 
 public class TimeFragment extends Fragment {
+    private RecyclerView timeRecyclerView;
+    private TimeItemAdapter timeItemAdapter;
+    private List<String> list;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.time_fragment, container, false);
+
+        timeRecyclerView = (RecyclerView) view.findViewById(R.id.time_recyclerview);
+        list.add("");
+        list.add("");
+        list.add("");
+        list.add("");
+        list.add("");
+        list.add("");
+        list.add("");
+        list.add("");
+        list.add("");
+        list.add("");
+        list.add("");
+        timeItemAdapter = new TimeItemAdapter(list);
+        timeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        timeRecyclerView.setAdapter(timeItemAdapter);
+
         return view;
     }
 }
