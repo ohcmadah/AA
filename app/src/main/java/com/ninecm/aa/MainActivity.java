7a4bb287baa09f7c0b71d2d563b6960349934575
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-08-22 19:50:18 +0900

    tablayout, viewpager listener 추가

--- app/src/main/java/com/ninecm/aa/MainActivity.java
+++ app/src/main/java/com/ninecm/aa/MainActivity.java
@@ -19,19 +19,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //init();
-        //setUp();
+        init();
+        setUp();
     }
 
     private void setUp() {
         tabLayout.setupWithViewPager(viewPager);
 
-        adapterPager = new MainAdapterPager(getSupportFragmentManager());
+        adapterPager = new MainAdapterPager(getSupportFragmentManager(), tabLayout.getTabCount());
         viewPager.setAdapter(adapterPager);
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(changePages);
     }
 
     private  void init() {
-        viewPager = findViewById(R.id.viewpager);
-        tabLayout = findViewById(R.id.tab_layout);
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        tabLayout = (TabLayout) findViewById(R.id.tab_layout);
     }
+
+    TabLayout.OnTabSelectedListener changePages = new TabLayout.OnTabSelectedListener() {
+        @Override
+        public void onTabSelected(TabLayout.Tab tab) {
+            viewPager.setCurrentItem(tab.getPosition());
+        }
+
+        @Override
+        public void onTabUnselected(TabLayout.Tab tab) {
+
+        }
+
+        @Override
+        public void onTabReselected(TabLayout.Tab tab) {
+
+        }
+    };
 }
