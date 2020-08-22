dfbb6500d55907add47262c27d9c30ff133cf3c6
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-08-22 21:05:24 +0900

    Time RecyclerView Adapter 생성

---
+++ app/src/main/java/com/ninecm/aa/adapter/TimeItemAdapter.java
@@ -0,0 +1,58 @@
+package com.ninecm.aa.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.ninecm.aa.R;
+
+import java.util.List;
+
+public class TimeItemAdapter extends RecyclerView.Adapter<TimeItemAdapter.ItemViewHolder> {
+
+    private List<String> list;
+
+    public TimeItemAdapter(List<String> list) {
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.id.time_item, viewGroup, false);
+        return new ItemViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemViewHolder holder, int position) {
+        if (position == 0) {
+            holder.timeItem.setBackgroundResource(R.drawable.round_corner_top);
+        }
+
+        if (position == list.size() - 1) {
+            holder.timeItem.setBackgroundResource(R.drawable.round_corner_bottom);
+            holder.divider.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    class ItemViewHolder extends RecyclerView.ViewHolder {
+        LinearLayout timeItem;
+        View divider;
+
+        public ItemViewHolder(View itemView) {
+            super(itemView);
+            timeItem = itemView.findViewById(R.id.time_item);
+            divider = itemView.findViewById(R.id.divider);
+        }
+    }
+
+}
