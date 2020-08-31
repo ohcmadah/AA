f06925243324d963b5677d86dfe76bd33bccadbd
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-08-31 18:05:30 +0900

    필요없는 import 제거, 주석 등 살짝 수정

--- app/src/main/java/com/ninecm/aa/fragment/RankingFragment.java
+++ app/src/main/java/com/ninecm/aa/fragment/RankingFragment.java
@@ -5,7 +5,6 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
@@ -38,9 +37,8 @@ public class RankingFragment extends Fragment {
         // init
         init(view);
 
-        // setUp
+        // set up
         setUp();
-
 
         return view;
     }
