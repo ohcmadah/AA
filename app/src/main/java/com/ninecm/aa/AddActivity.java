a3ba114871ba6b1f1eab2aa3d041f3581052283a
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-09-01 23:13:00 +0900

    add 화면 개봉일 출력

--- app/src/main/java/com/ninecm/aa/AddActivity.java
+++ app/src/main/java/com/ninecm/aa/AddActivity.java
@@ -1,15 +1,53 @@
 package com.ninecm.aa;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.BreakIterator;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
 
 public class AddActivity extends AppCompatActivity {
+
+    DatePicker picker;
+    Button btnGet;
+    TextView tvw;
+    Calendar c;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
         getWindow().clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN);
+
+        c = Calendar.getInstance();
+        int year = c.get(c.YEAR);
+        int month = c.get(c.MONTH);
+        int dayOfMonth = c.get(c.DAY_OF_MONTH);
+
+        tvw = (TextView) findViewById(R.id.textView1);
+        picker = (DatePicker) findViewById(R.id.datePicker);
+
+        btnGet=(Button)findViewById(R.id.button);
+        btnGet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvw.setText(picker.getYear() + "년 " + (picker.getMonth()+1) + "월 " + picker.getDayOfMonth() + "일");
+            }
+        });
     }
+}
