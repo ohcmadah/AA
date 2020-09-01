2ee46227beea2348ce1c0c1b267277cda918e8b3
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-09-01 09:52:06 +0900

    backdrop 안됨

--- app/src/main/java/com/ninecm/aa/MainActivity.java
+++ app/src/main/java/com/ninecm/aa/MainActivity.java
@@ -1,16 +1,23 @@
 package com.ninecm.aa;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
 import com.ninecm.aa.adapter.MainAdapterPager;
+import com.roacult.backdrop.BackdropLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     private TabLayout tabLayout;
     private FloatingActionButton fab;
     private ImageButton btnSearch;
+    private BackdropLayout backdropLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
         btnSearch = (ImageButton) findViewById(R.id.btn_search);
     }
 
+
     private void setUp() {
         // tabLayout을 viewPager와 함께 동작(연동)
         tabLayout.setupWithViewPager(viewPager);
@@ -50,7 +59,9 @@ public class MainActivity extends AppCompatActivity {
 
         // 검색 버튼 클릭 ActionListener
         btnSearch.setOnClickListener(goSearchPage);
+        fab.setOnClickListener(goAddPage);
     }
+
 
     // 상단 Navigation ActionListener
     TabLayout.OnTabSelectedListener changePage = new TabLayout.OnTabSelectedListener() {
@@ -81,6 +92,17 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View view) {
             Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+            startActivity(intent);
+            overridePendingTransition(R.anim.anim_slide_in_left, R.anim.anim_slide_out_right);
+            MainActivity.this.finish();
+        }
+    };
+
+    // backdrop 안돼서 일단 클릭하면 옆으로 넘어가는걸로 대채함..
+    View.OnClickListener goAddPage = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(MainActivity.this, AddActivity.class);
             startActivity(intent);
             overridePendingTransition(R.anim.anim_slide_in_left, R.anim.anim_slide_out_right);
             MainActivity.this.finish();
