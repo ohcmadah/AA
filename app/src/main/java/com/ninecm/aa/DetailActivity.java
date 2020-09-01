46be039167e7f339659cc07fa5992a0fa2227a86
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-09-01 16:49:37 +0900

    뒤로가기 버튼 액션 추가

--- app/src/main/java/com/ninecm/aa/DetailActivity.java
+++ app/src/main/java/com/ninecm/aa/DetailActivity.java
@@ -2,13 +2,36 @@ package com.ninecm.aa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class DetailActivity extends AppCompatActivity {
+    private ImageButton btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        init();
+
+        setUp();
     }
+
+    private void init() {
+        btnBack = (ImageButton) findViewById(R.id.btn_back);
+    }
+
+    private void setUp() {
+        btnBack.setOnClickListener(goBackPage);
+    }
+
+    View.OnClickListener goBackPage = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            DetailActivity.this.finish();
+        }
+    };
 }
