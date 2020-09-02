18b7504b451908ed846b35ad381a0944dcadce45
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-09-03 01:08:39 +0900

    detail페이지 star_icon 동적 추가

--- app/src/main/java/com/ninecm/aa/DetailActivity.java
+++ app/src/main/java/com/ninecm/aa/DetailActivity.java
@@ -2,26 +2,50 @@ package com.ninecm.aa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.app.ActionBar;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import static com.ninecm.aa.R.*;
+
 
 public class DetailActivity extends AppCompatActivity {
     private ImageButton btnBack;
+    private int star_number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
+        setContentView(layout.activity_detail);
 
         init();
-
         setUp();
+
+        ImageView iv = new ImageView(this);
+        LinearLayout layout = (LinearLayout) findViewById(id.star_container);
+
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+
+        iv.setLayoutParams(new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT)
+        );
+        iv.setImageResource(drawable.star_icon);
+
+        layout.addView(iv);
+
     }
 
     private void init() {
-        btnBack = (ImageButton) findViewById(R.id.btn_back);
+        btnBack = (ImageButton) findViewById(id.btn_back);
     }
 
     private void setUp() {
