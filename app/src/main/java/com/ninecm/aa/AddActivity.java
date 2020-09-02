c9b5dcd31ee7a1971bc1fd2ded08cb844ed19714
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-09-02 21:45:59 +0900

    취소 버튼 액션 추가

--- app/src/main/java/com/ninecm/aa/AddActivity.java
+++ app/src/main/java/com/ninecm/aa/AddActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.util.Calendar;
@@ -15,16 +16,23 @@ import java.util.Calendar;
 
 public class AddActivity extends AppCompatActivity {
 
-    DatePicker picker;
-    Button btnGet;
-    TextView tvw;
-    Calendar c;
+    private DatePicker picker;
+    private Button btnGet;
+    private TextView tvw;
+    private Calendar c;
+    private ImageButton btnCancel;
+    private Button btnAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
         getWindow().clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN);
+
+        btnCancel = (ImageButton) findViewById(R.id.btn_cancel);
+        btnAdd = (Button) findViewById(R.id.btn_add);
+
+        btnCancel.setOnClickListener(closePage);
 
         OpenDatePicker();
     }
@@ -47,6 +55,14 @@ public class AddActivity extends AppCompatActivity {
         public void onClick(View v) {
             Log.d("MyTag", "Click");
             tvw.setText(picker.getYear() + "년 " + (picker.getMonth()+1) + "월 " + picker.getDayOfMonth() + "일");
+        }
+    };
+
+    View.OnClickListener closePage = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            AddActivity.this.finish();
+            overridePendingTransition(R.anim.anim_stay, R.anim.anim_slide_down);
         }
     };
 }
