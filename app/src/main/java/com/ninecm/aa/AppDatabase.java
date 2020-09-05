855a920ef91fe47e88fc6cc8331010d290b78b67
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-09-05 23:02:48 +0900

    필요없는 import 제거

--- app/src/main/java/com/ninecm/aa/AppDatabase.java
+++ app/src/main/java/com/ninecm/aa/AppDatabase.java
@@ -1,13 +1,9 @@
 package com.ninecm.aa;
 
 import android.content.Context;
-import android.os.AsyncTask;
-
-import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
-import androidx.sqlite.db.SupportSQLiteDatabase;
 
 @Database(entities = {Cosmetic.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
