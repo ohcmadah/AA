25bed8ea4f25231a19326b2c06613ffdc20ee138
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-09-05 23:02:29 +0900

    callback 제거

--- app/src/main/java/com/ninecm/aa/AppDatabase.java
+++ app/src/main/java/com/ninecm/aa/AppDatabase.java
@@ -18,31 +18,8 @@ public abstract class AppDatabase extends RoomDatabase {
         if (INSTANCE == null) {
             INSTANCE = Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class, "cosmetic-db")
                     .fallbackToDestructiveMigration()
-//                    .addCallback(roomCallback)
                     .build();
         }
         return INSTANCE;
-    }
-
-    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback() {
-        @Override
-        public void onCreate(@NonNull SupportSQLiteDatabase db) {
-            super.onCreate(db);
-            new PopulateDbAsyncTask(INSTANCE).execute();
-        }
-    };
-
-    private static class PopulateDbAsyncTask extends AsyncTask<Void, Void, Void> {
-        private CosmeticDao cosmeticDao;
-        private PopulateDbAsyncTask(AppDatabase db) {
-            cosmeticDao = db.cosmeticDao();
-        }
-        @Override
-        protected Void doInBackground(Void... voids) {
-            cosmeticDao.insert(new Cosmetic("Cosmetic 1", "20200930", 3, "hi!"));
-            cosmeticDao.insert(new Cosmetic("Cosmetic 2", "20201002", 3, "oh!"));
-            cosmeticDao.insert(new Cosmetic("Cosmetic 3", "20201203", 2, "my!"));
-            return null;
-        }
     }
 }
