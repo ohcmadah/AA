e227e2fcdbdf4bfe0e8e766aa6a2b54e8e6a9214
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-10-05 14:01:01 +0900

    xml에서 imagebutton 잘못 설정한 것 삭제

--- app/src/main/java/com/ninecm/aa/fragment/RankingFragment.java
+++ app/src/main/java/com/ninecm/aa/fragment/RankingFragment.java
@@ -104,27 +104,28 @@ public class RankingFragment extends Fragment {
         @Override
         public void onClick(View view) {
             List<Cosmetic> changedCosmetics = new ArrayList<>();
+            btnThreeStar.setSelected(false);
             switch (view.getId()) {
                 case R.id.btn_three_star:
-                    btnTwoStar.setSelected(false);
-                    btnOneStar.setSelected(false);
-                    btnThreeStar.setSelected(true);
+//                    btnTwoStar.setSelected(false);
+//                    btnOneStar.setSelected(false);
+//                    btnThreeStar.setSelected(true);
 
                     changedCosmetics = calStarPage(cosmetics, 3);
                     break;
 
                 case R.id.btn_two_star:
-                    btnThreeStar.setSelected(false);
-                    btnOneStar.setSelected(false);
-                    btnTwoStar.setSelected(true);
+//                    btnThreeStar.setSelected(false);
+//                    btnOneStar.setSelected(false);
+//                    btnTwoStar.setSelected(true);
 
                     changedCosmetics = calStarPage(cosmetics, 2);
                     break;
 
                 case R.id.btn_one_star:
-                    btnThreeStar.setSelected(false);
-                    btnTwoStar.setSelected(false);
-                    btnOneStar.setSelected(true);
+//                    btnThreeStar.setSelected(false);
+//                    btnTwoStar.setSelected(false);
+//                    btnOneStar.setSelected(true);
 
                     changedCosmetics = calStarPage(cosmetics, 1);
                     break;
