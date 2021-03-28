0624da89a8776dd1b4f7bcdd39d9f0faaf182ec0
cho-hadam <s2019w16@e-mirim.hs.kr>
2021-03-28 16:58:06 +0900

    주석 작성

--- app/src/main/java/com/ninecm/aa/Calculator.java
+++ app/src/main/java/com/ninecm/aa/Calculator.java
@@ -71,17 +71,24 @@ public class Calculator {
     }
 
     // Emergency 계산
+    // Emergency란 가장 상단에 강조되어 나타나는 유통기한 임박 화장품 1개를 말한다.
     public static int getEmergIndex(List<Cosmetic> cosmetics) {
+        // 만약 화장품 객체 리스트에 데이터가 아예 없다면, -1 리턴 (없다는 뜻)
         int emergIndex = -1;
+        // 계산할 화장품 객체가 리스트 내에 있다면
         if (cosmetics.size() != 0) {
+            // 화장품 당 디데이 수를 저장하는 배열 선언
             int[] dCountList = new int[cosmetics.size()];
+
             for (int i = 0; i < cosmetics.size(); i++) {
+                // 화장품 각각의 유통기한으로 Calculator 객체 세팅 (직접 만든 계산 클래스)
                 Calculator calculator = Calculator.setCalculator(cosmetics, i);
 
                 // D-Day 계산
                 dCountList[i] = calculator.calDday();
             }
 
+            // 유통기한이 가장 적게 남은 것(최솟값) 구하기
             int min = dCountList[0];
             for (int j = 0; j < dCountList.length; j++) {
                 if (min > dCountList[j]) {
@@ -89,6 +96,7 @@ public class Calculator {
                 }
             }
 
+            // 구한 최솟값과 남은 날짜가 같다면 Emergency이므로 인덱스 저장
             for (int k = 0; k < dCountList.length; k++) {
                 if (min == dCountList[k]) {
                     emergIndex = k;
@@ -96,6 +104,7 @@ public class Calculator {
             }
         }
 
+        // -1 또는 구한 Emergency index 값 return
         return emergIndex;
     }
 
